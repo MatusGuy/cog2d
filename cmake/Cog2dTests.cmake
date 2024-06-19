f5eac81eb31d8ec7fe8f0c81ab5b8063d9cf890f
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2024-06-19 16:21:58 +0000

    cog2d for windows

--- cmake/Cog2dTests.cmake
+++ cmake/Cog2dTests.cmake
@@ -6,4 +6,13 @@ macro(cog2d_add_test name)
 	add_executable(${name} ${test_src})
 
 	target_link_libraries(${name} PUBLIC cog2d)
+
+	if(WIN32)
+		add_custom_command(TARGET ${name} POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -E copy
+			$<TARGET_RUNTIME_DLLS:${name}>
+			$<TARGET_FILE_DIR:${name}>
+			COMMAND_EXPAND_LISTS
+		)
+	endif()
 endmacro()
