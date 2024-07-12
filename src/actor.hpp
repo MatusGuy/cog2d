cbaffe0f54bf0204dcbf4d8feafb255ffc1626fe
MatusGuy <MatusGuy@users.noreply.github.com>
2024-07-12 23:32:07 +0100

    more docs

--- src/actor.hpp
+++ src/actor.hpp
@@ -4,10 +4,10 @@
 #include "types.hpp"
 
 /*!
- * \brief The Actor class
+ * \brief A moving object on a scene
  *
- * The Actor class represents a moving object on a
- * scene.
+ * It can be used to represent a player, an enemy, or
+ * just a normal object in your game.
  */
 class Actor
 {
@@ -15,13 +15,26 @@ public:
 	Actor();
 
 	/*!
-	 * \brief update
-	 *
 	 * Updates the position of the actor for the next frame.
 	 * This is called automatically by the \ref ActorManager
 	 */
 	virtual void update();
+
+	/*!
+	 * Draws the actor in whatever way you'd like.
+	 * This is called automatically by the \ref ActorManager
+	 */
 	virtual void draw() = 0;
+
+	/*!
+	 * \return The bounding box
+	 */
+	inline Rect& get_bbox() { return m_bbox; }
+
+	/*!
+	 * \return The bounding vel
+	 */
+	inline Vector& get_vel() { return m_vel; }
 
 protected:
 	Rect m_bbox;
--- src/actormanager.hpp
+++ src/actormanager.hpp
@@ -5,12 +5,27 @@
 
 #include "actor.hpp"
 
+
+/*!
+ * \class ActorManager
+ * \brief Controls all actors
+ *
+ * This class controls all \ref Actor instances.
+ * They are updated & drawn by the \ref Program.
+ */
 #define COG2D_USE_ACTORMANAGER COG2D_USING(ActorManager, actormanager)
 class ActorManager : public Singleton<ActorManager>
 {
 public:
 	ActorManager();
 
+	/*!
+	 * This function creates an \ref Actor and adds it immediately to
+	 * the \ref ActorManager.
+	 *
+	 * \param args Arguments passed to the constructor
+	 * \return Pointer to the fresh new \ref Actor
+	 */
 	template<class T, typename... Args>
 	T* create(Args&&... args)
 	{
@@ -19,6 +34,9 @@ public:
 		return actor;
 	}
 
+	/*!
+	 * \return All actors in the manager.
+	 */
 	inline const std::vector<Actor*>& get_actors() const {
 		return m_actors;
 	}
