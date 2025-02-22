a819830a06292bc87c4b1d666093f7eb118e326b
Prune Juice <tylerandari12@gmail.com>
2025-02-21 18:37:10 -0600

    Now you have gravity in cog2d.

--- src/actor.cpp
+++ src/actor.cpp
@@ -1,12 +1,23 @@
 #include "actor.hpp"
 
-Actor::Actor():
-	CollisionBody(),
-	m_vel(0, 0)
-{
-}
+#include "program.hpp"
+
+Actor::Actor()
+    : CollisionBody()
+    , m_vel(0, 0)
+    , m_accel(0, 0)
+{}
 
 void Actor::update()
 {
-	m_mov = m_vel;
+    if (m_grav != 0)
+        gravity();
+    m_mov = m_vel;
+}
+
+void Actor::gravity()
+{
+    // This function hates operator overloads, so just add the x and y seperately.
+    m_vel.x += m_accel.x * Program::get().m_delta_time;
+    m_vel.y += (m_accel.y + m_grav) * Program::get().m_delta_time;
 }
