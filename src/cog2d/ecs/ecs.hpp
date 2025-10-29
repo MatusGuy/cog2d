b7bafa880b7185db2a3331df528aef6bc2a8e692
MatusGuy <MatusGuy@users.noreply.github.com>
2025-10-29 21:53:31 +0000

    Add ecs class

---
+++ src/cog2d/ecs/ecs.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstring>
+
+namespace cog2d {
+
+template<class E>
+class Ecs
+{
+public:
+	static constexpr int MAX_ENTITIES = 100;
+
+public:
+	Ecs() {}
+
+	E& create()
+	{
+		E& out = m_entities[m_num_entities];
+		out = E{};
+		++m_num_entities;
+		return out;
+	}
+
+	void clear() { std::memset(m_entities, 0, sizeof(m_entities)); }
+
+	inline E* entities() { return m_entities; }
+	inline int num_entities() { return m_num_entities; }
+
+	E& operator[](int idx) { return entities()[idx]; }
+
+private:
+	E m_entities[MAX_ENTITIES] = {0};
+	int m_num_entities;
+};
+
+}  //namespace cog2d
